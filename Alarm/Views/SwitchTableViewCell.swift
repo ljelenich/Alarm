420e54244f0ff9023aa3f7d4628ca2de66707da4
LAURA JELENICH <laurajelenich@LAURAs-MBP-2.lan1>
2020-09-14 16:08:44 -0400

    First

---
+++ Alarm/Views/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by LAURA JELENICH on 9/14/20.
+//  Copyright © 2020 LAURA JELENICH. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    
+    @IBAction func switchValueChanged(_ sender: Any) {
+    }
+    
+    
+}
